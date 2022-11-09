3bfec271f59220030a373477c1b1f6b447e9b94d
Viktor Semenov <vsemenov@kode-t.ru>
2022-11-09 23:47:41 +0700

    Initial Commit

---
+++ FileAuditSystem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileAuditSystem
+//
+//  Created by Виктор Семенов on 09.11.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
